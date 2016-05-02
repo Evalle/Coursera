3e5a11a71d7dcf84e9c86cc593f8088abbec22d1
evalle <shmarnev@gmail.com>
2016-05-02 22:05:50 +0200

    Add code for 10.4

--- datastructures/10_4.py
+++ datastructures/10_4.py
@@ -3,3 +3,22 @@
 From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
 Once you have accumulated the counts for each hour, print out the counts, sorted by hour as shown below.
 '''
+name = raw_input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+handle = open(name)
+lst = list()
+lst2 = list()
+lst3 = list()
+counts = dict()
+for line in handle:
+    if line.startswith('From '):
+        lst.append(line.split())
+for i in lst:
+    lst2.append(i[5].split(':'))
+for i in lst2:
+    lst3.append(i[0])
+for hour in lst3:
+    counts[hour] = counts.get(hour,0)+1
+t = sorted(counts.items())
+for (k,v) in t:
+    print(k,v)
