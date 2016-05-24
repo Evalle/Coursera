6d8dc92c423baa7064a74df96ca7e681addd24cd
evalle <shmarnev@gmail.com>
2016-05-24 17:13:11 +0200

    Added final solution

--- using_python_to_access_webdata/week_04/beauty.py
+++ using_python_to_access_webdata/week_04/beauty.py
@@ -1,14 +1,14 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import re
 
-#url = input('Enter - ')
-html = urlopen('http://python-data.dr-chuck.net/comments_42.html')
+url = input('Enter - ')
+html = urlopen(url).read()
 soup = BeautifulSoup(html, "html.parser")
 tags = soup('span')
 
+sum = 0
+
 for tag in tags:
-#    print('TAG:', tag)
-#    print('URL:', tag.get('href', None))
-    print('Contents:', tag.contents[0])
-#    print('Attrs:', tag.attrs)
+   # Look at the parts of a tag
+   sum += (int(tag.contents[0]))
+print(sum)
