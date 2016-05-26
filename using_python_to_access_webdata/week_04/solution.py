ec1de6bce21b252b104510b5c8eb92eaa413a7b5
evalle <shmarnev@gmail.com>
2016-05-26 18:05:43 +0200

    Added function to the solution

--- using_python_to_access_webdata/week_04/solution.py
+++ using_python_to_access_webdata/week_04/solution.py
@@ -2,12 +2,25 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 url = input('Enter URL: ')
-count = input('Enter count: ')
-position = input('Enter: ')
+#count = int(input('Enter count: '))
+position = int(input('Enter position: '))
 
-html = urlopen(url).read()
-soup = BeautifulSoup(html, "html.parser")
-tags = soup('span')
+def followtheleader(url, position):
+    html = urlopen(url).read()
+    soup = BeautifulSoup(html, "html.parser")
+    lst = list()
 
-for tag in tags:
-   sum += (int(tag.contents[0]))
+    for link in soup.find_all('a'):
+        lst.append((link.get('href')))
+
+    return(lst[position-1])
+
+print(followtheleader(url, position))
+
+#for tag in tags:
+#    print('TAG:', tag)
+#     print('URL:', tag.get('href', None))
+#    print('Contents:', tag.contents[0])
+#    print('Attrs:', tag.attrs)
+
+#tag.get('href', )
