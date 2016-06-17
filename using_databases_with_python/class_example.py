82de57d44926174bc1b3ead097561cf54a95e268
evalle <shmarnev@gmail.com>
2016-06-17 15:35:42 +0200

    Added destructor and constructor

--- using_databases_with_python/class_example.py
+++ using_databases_with_python/class_example.py
@@ -1,10 +1,16 @@
 class PartyAnimal:
 
     x = 0 
+    
+    def __init__(self):
+        print("I'm constructed")
 
     def party(self):
         self.x += 1
         print("So far", self.x)
+
+    def __del__(self):
+        print("I'm destructed", self.x)
 
 an = PartyAnimal()
 
